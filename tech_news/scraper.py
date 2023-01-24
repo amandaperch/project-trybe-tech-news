5d56c365f1332564292d0e20b19389941d3609c2
Amanda Perch <perch_amanda@hotmail.com>
2023-01-24 14:55:12 -0300

    Função scrape_next_page_link implementada

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next = selector.css('a.next::attr(href)').get()
+    return next if next else None
 
 
 # Requisito 4
