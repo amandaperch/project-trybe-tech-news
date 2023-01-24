f8231f77c14b37bf846c5086f7149749a44430f2
Amanda Perch <perch_amanda@hotmail.com>
2023-01-24 15:27:13 -0300

    função search_by_title implementada

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,17 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    title = search_news({'title': {"$regex": title, "$options": 'i'}})
+    return [(titles['title'], titles['url']) for titles in title]
 
+
+# [
+#   ("Título1_aqui", "url1_aqui"),
+#   ("Título2_aqui", "url2_aqui"),
+#   ("Título3_aqui", "url3_aqui"),
+# ]
 
 # Requisito 7
 def search_by_date(date):
